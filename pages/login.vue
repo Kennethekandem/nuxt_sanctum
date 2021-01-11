8051549e96a195d8af8ede800bb66de54281462f
Kenneth Ekandem <kennyekandem@gmail.com>
2021-01-11 17:06:02 +0100

    update

---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div>
+        <label>Username</label>
+        <input type="text" v-model="login.username" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="text" v-model="login.password" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+
+    data() {
+      return {
+        login: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      async userLogin() {
+        await this.$axios.get('/sanctum/csrf-cookie', {
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+          },
+          withCredentials: true,
+        }).then(response => {
+          console.log(response);
+        })
+      }
+    }
+  }
+</script>
